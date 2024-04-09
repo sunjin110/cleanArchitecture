e31865463b614fa38f5bb14e06c55691f690f240
sunjin110 <sunjin110@gmail.com>
2024-04-09 10:41:07 +0800

    a

--- src/main/java/info/sunjin/cleanArchitecture/presenteter/restapi/RestApi.java
+++ src/main/java/info/sunjin/cleanArchitecture/presenteter/restapi/RestApi.java
@@ -1,9 +1,7 @@
 package info.sunjin.cleanArchitecture.presenteter.restapi;
 
-import info.sunjin.cleanArchitecture.domain.repository.ArticleRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class RestApi {
     private static final Logger logger = LogManager.getLogger(RestApi.class);
-
-    @Autowired
-    ArticleRepository articleRepo;
 
     @GetMapping("/greeting")
     public String sayHello() {
--- src/test/java/info/sunjin/cleanArchitecture/CleanArchitectureApplicationTests.java
+++ src/test/java/info/sunjin/cleanArchitecture/CleanArchitectureApplicationTests.java
@@ -2,12 +2,14 @@ package info.sunjin.cleanArchitecture;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan
 class CleanArchitectureApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	 @Test
+	 void contextLoads() {
+	 }
 
 }
